3605d520521c56cb919a6baf347460f108678f65
sumastavr <pamungkas@sumasta.tech>
2021-08-05 01:33:29 +0200

    init commit duplicating from old folder and rename

---
+++ src/sumastaConfig.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+// SW Version
+const String SWVersion = "V1.31 (28-07-21) P.Sumasta";
+
+// Function prototype
+float getLat();
+int getUplinkSpeed();
+int getDownlinkSpeed();
